ec190c2d1fbe56411f6d80f0926ceb1e76ed7d69
Noah Marsh <noahjmarsh@gmail.com>
2019-11-08 22:49:17 -0500

    Updates

---
+++ precache-manifest.9b20cb95b7d714d095140cbf624f2e85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcff24d8e3f53f1744d98a470b148cbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0704c8901754eb3d7f19",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "80e8095cb01552739dad",
+    "url": "/static/js/2.2613f6c5.chunk.js"
+  },
+  {
+    "revision": "0704c8901754eb3d7f19",
+    "url": "/static/js/main.00c0b0a0.chunk.js"
+  },
+  {
+    "revision": "371e22fa3947fc22a22d",
+    "url": "/static/js/runtime-main.b224fbd3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
